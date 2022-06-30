42d285b828dbcd60c8f223a38b7fd56fefcd1a92
iamsps1995 <iamsps1995>
2022-06-30 10:46:04 +0530

    newly added

---
+++ Array2d.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+//2D-Array
+public class Array2d {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first square bracket value: (row)(m)");
+        int m = scan.nextInt();
+        System.out.println("Enter the second square bracket value: (col)(n)");
+        int n = scan.nextInt();
+        
+        int a[][] = new int[m][n];
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                System.out.println("Enter the value:");
+                a[i][j] = scan.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                System.out.print("The value is: ");
+                System.out.println(a[i][j]);
+            }
+        }
+    }
+}
